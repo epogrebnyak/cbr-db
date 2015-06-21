f6ba4fd72d423ec2de4874d469d20a91152eea2e
=epogrebnyak <e.pogrebnyak@gmail.com>
2015-06-21 05:17:45 +0300

    Created test script for form 102

---
+++ py3/utils/test-one-date-102.bat
@@ -0,0 +1,16 @@
+exec ini.bat
+cd ..
+python bankform.py reset database raw
+python bankform.py reset database final
+python bankform.py download  102 2015Q1
+python bankform.py unpack  102 2015Q1
+python bankform.py make csv  102 2015Q1
+python bankform.py import csv 102 2015Q1
+python bankform.py make dataset 102
+python bankform.py migrate dataset 102
+python bankform.py import alloc
+python bankform.py import tables
+python bankform.py make balance
+python bankform.py test balance
+python bankform.py report balance
+
