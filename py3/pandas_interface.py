6996f478c00b45eff8a903716385d8ab19600041
bhanu860 <bhanupratapsingh.860@gmail.com>
2015-06-27 00:56:34 +0200

    step_1 complete implementation

--- py3/pandas_interface.py
+++ py3/pandas_interface.py
@@ -38,30 +38,29 @@ def insert_entries(balancedf):
     pandas equivalent of sql procedure balance_make_saldo_198_298 
     '''
     #subset dataframes
-    a = balancedf[balancedf['line']==198000]
-    b = balancedf[balancedf['line']==298000]
+    tempdf = balancedf.reset_index()
+    a = tempdf[tempdf['line']==198000]
+    b = tempdf[tempdf['line']==298000]
     
     #merge dataframes
-    saldo = a.merge(b, on=['dt', 'regn'], how='left')    
+    saldo = a.merge(b, on=['dt', 'regn'], how='left', suffixes=('_a', '_b'))    
     
     #assign values to iv, ir and itogo as per the cases
-    saldo_ext = pd.DataFrame(np.zeros(saldo.shape[0],3), 
+    saldo_ext = pd.DataFrame(np.zeros((saldo.shape[0],3)), 
                              columns=['ir', 'iv', 'itogo'])
     saldo_ext.index = saldo.index
     
-    #set ir column values
-    saldo_ext.ix[saldo['ir_x'] > saldo['ir_y'],'ir'] = (saldo.ix[saldo['ir_x'] > saldo['ir_y'],'ir_x'] - saldo.ix[saldo['ir_x'] > saldo['ir_y'],'ir_y']).values
-    saldo_ext.ix[saldo['ir_x'] < saldo['ir_y'],'ir'] = (saldo.ix[saldo['ir_x'] < saldo['ir_y'],'ir_y'] - saldo.ix[saldo['ir_x'] < saldo['ir_y'],'ir_x']).values
-    
-    #set iv column values
-    saldo_ext.ix[saldo['iv_x'] > saldo['iv_y'],'iv'] = (saldo.ix[saldo['iv_x'] > saldo['iv_y'],'iv_x'] - saldo.ix[saldo['iv_x'] > saldo['iv_y'],'iv_y']).values
-    saldo_ext.ix[saldo['iv_x'] < saldo['iv_y'],'iv'] = (saldo.ix[saldo['iv_x'] < saldo['iv_y'],'iv_y'] - saldo.ix[saldo['iv_x'] < saldo['iv_y'],'iv_x']).values
-    
+    #set ir column values    
+    for col in ['ir', 'iv']:
+        cola = col+'_a'; colb = col+'_b'
+        a_gt_b = saldo[cola] > saldo[colb]
+        a_lt_b = saldo[cola] < saldo[colb]        
+        saldo_ext.ix[a_gt_b, col] = (saldo.ix[a_gt_b,cola] - saldo.ix[a_gt_b, colb]).values
+        saldo_ext.ix[a_lt_b, col] = (saldo.ix[a_lt_b,colb] - saldo.ix[a_lt_b, cola]).values
+        
     #set itogo column values
     saldo_ext.ix[:,'itogo'] = (saldo_ext.ix[:,'ir'] + saldo_ext.ix[:, 'iv']).values
 
-    #stack saldo_ext to saldo i.e. add ir, iv and itogo updated columns
-    saldo = pd.tools.merge.concat([saldo, saldo_ext], axis=1)
     
     #select relevant columns from saldo
     #TODO: 
@@ -70,8 +69,8 @@ def insert_entries(balancedf):
     #TODO: 
     
     #delete rows from balacedf for lines 198K and 298K
-    balancedf = balancedf[balancedf.line != 198000]
-    balancedf = balancedf[balancedf.line != 298000]
+    tempdf = tempdf[tempdf.line != 198000]
+    tempdf = tempdf[tempdf.line != 298000]
     
     #insert rows in balancedf using saldo dataframe
     #TODO:
@@ -87,19 +86,47 @@ def insert_totals(balancedf):
     pass
 
 
+def init_balancedf():
+    '''
+    Initializes balance dataframe from alloc and f101 tables, 
+    Equivalent to 'balance_make_step_1' sql procedure
+    '''
+    #left join alloc and f101 on 'conto' to create initial balance data frame
+    balancedf = alloc.merge(f101, on='conto', how='left')    
+    
+    #drop rows with null conto values
+    balancedf = balancedf[~balancedf['conto'].isnull()]
+    
+    #ir, iv, itogo multipy with mult
+    balancedf['ir'] = balancedf['ir']*balancedf['mult']
+    balancedf['iv'] = balancedf['iv']*balancedf['mult']
+    balancedf['itogo'] = balancedf['itogo']*balancedf['mult']
+    
+    #groupby dt, line and regn and sum
+    balancedf_grp = balancedf.groupby(['dt', 'line', 'regn'], sort=False)
+    balancedf_grpdf = balancedf_grp.agg({'ir':sum,'iv':sum,'itogo':sum})
+    
+    #take relevant columns, la_p, ha_iv and lev
+    balancedf = balancedf[['dt', 'line', 'regn', 'lev', 'la_p', 'has_iv']]
+    
+    #index on dt, line and regn
+    balancedf = balancedf.set_index(keys=['dt', 'line', 'regn'])
+    
+    #add remaining columns from the grouped sum
+    balancedf = balancedf.join(balancedf_grpdf)
+    #TODO: check if we need to remove duplicates, need original sql balance table for that
+    
+    return balancedf
+
+
 def make_balance():
     '''
     Pandas equivalent of make_balance procedure. It uses 'f101' and 'alloc' tables
     read from database to create 'balance' dataframe. 
     @return: 'balance' dataframe  
     '''
-    #left join alloc and f101 on 'conto' to create initial balance data frame
-    balancedf = alloc.merge(f101, on='conto', how='left')    
-    #TODO: check if v.conto is null, if yes then drop those rows, need some data in tables for this
-    #TODO: groupby and aggregate
-    balancedf = balancedf.ix[:,['dt', 'line', 'lev', 'la_p', 'regn', 'has_iv', 
-                                'ir', 'iv', 'itogo']]
-    
+    #initialize balance dataframe from alloc and f101
+    balancedf = init_balancedf()        
     
     #balance_make_saldo_198_298
     balancedf = insert_entries(balancedf)
@@ -107,15 +134,7 @@ def make_balance():
     #balance_make_insert_totals
     balancedf = insert_totals(balancedf)
     
-
     return balancedf
-
-
-
-for var in [f101, alloc, balance]:
-    print()
-    print(var.head())
-    pprint(var.columns.tolist())
 
 
 
