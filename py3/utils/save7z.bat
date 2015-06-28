ecc8a5ca415497ff7f1a7983d3eb15207f15ad8b
=epogrebnyak <e.pogrebnyak@gmail.com>
2015-06-29 02:14:33 +0300

    Moved save7z.bat to 'utils' folder

--- py3/utils/save7z.bat
+++ py3/utils/save7z.bat
@@ -4,5 +4,9 @@ REM 10:12 18.06.2015
 if []==[%1] (set "filename=ep_bf.7z" 
   ) else (set "filename=%1")
 
+REM Move to root folder
+cd..
+cd..
+
 del %filename%
 bin\7za a %filename% * -xr!data.downloadable -xr!output -xr!data.private -x!%filename% -xr!py3\__pycache__\
