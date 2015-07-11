0508002a21b3a7a9a305b5fa68d800a9212f0a4e
repo2156 <repo2156@gmail.com>
2015-07-11 20:40:35 -0300

    Added baf.sh

--- py3/baf.bat
+++ py3/baf.bat
@@ -2,5 +2,4 @@
 REM Short-call wrapper for python bankform.py
 REM Allows to call 'baf reset database' instead of 'python bankform.py reset database' 
 REM This saves a bit of time typing.
-REM Todo: need similar bash script 'baf.sh'
 python bankform.py %1 %2 %3 %4 %5 %6 %7 %8
