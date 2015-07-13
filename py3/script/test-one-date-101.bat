7556b4fd5c2c281e2b61ce95d54108269cdd607e
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-13 12:56:57 +0300

    Update test-one-date-101.bat

--- py3/script/test-one-date-101.bat
+++ py3/script/test-one-date-101.bat
@@ -1,10 +1,10 @@
 cd ..
 python bankform.py reset database raw
 python bankform.py reset database final
-python bankform.py download   101 2015-01-01
-python bankform.py unpack     101 2015-01-01
-python bankform.py make csv   101 2015-01-01
-python bankform.py import csv 101 2015-01-01
+python bankform.py download     101 2015-01-01
+python bankform.py unpack       101 2015-01-01
+python bankform.py make csv     101 2015-01-01
+python bankform.py import csv   101 2015-01-01
 python bankform.py make dataset 101 2015-01-01
 python bankform.py migrate dataset 101
 python bankform.py import alloc
