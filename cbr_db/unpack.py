501db74d2463e13bbcbda768f3f516b9da978aae
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2016-01-17 16:08:58 +0500

    unpack: using ZipFile instead of 7z

--- cbr_db/unpack.py
+++ cbr_db/unpack.py
@@ -1,4 +1,5 @@
 import os
+from zipfile import ZipFile
 
 from .terminal import terminal
 from .config_folders import get_public_data_folder, PATH
@@ -11,21 +12,45 @@ def get_local_ziprar_filepath(isodate, form):
     print("File:", filename)
     return os.path.join(dir_, filename)
 
+
 def unpack(isodate, form):
     # not todo - may omit 'destination_directory' form here
     filepath = get_local_ziprar_filepath(isodate, form)
     unpack_path(filepath, form)
 
+
 def unpack_path(filepath, form):
     destination_directory = get_public_data_folder(form, 'dbf')
-    ext = os.path.splitext(filepath)[1].lower()
-
-    if ext == '.rar':
-        call_string = " ".join([in_quotes(PATH['unrar']), "e", filepath, destination_directory, "-y"])
+    ext = os.path.splitext(filepath)[1].lstrip('.').lower()
+    # Make sure target directory exists.
+    # NOTE: `unrar e` may do nothing if target directory does not exist.
+    if not os.path.isdir(destination_directory):
+        os.makedirs(destination_directory)
+    if ext == 'rar':
+        _unpack_rar(filepath, destination_directory)
+    elif ext == 'zip':
+        _unpack_zip(filepath, destination_directory)
+    elif ext == '7z':
+        _unpack_7z(filepath, destination_directory)
     else:
-        call_string = " ".join([in_quotes(PATH['z7']), "e", filepath, "-o" + destination_directory, "-y"])
+        raise Exception('Unsupported archive type: {}'.format(filepath))
 
+
+def _unpack_rar(filepath, destination_dir):
+    call_string = " ".join([in_quotes(PATH['unrar']), "e", filepath, destination_dir, "-y"])
     terminal(call_string)
+
+
+def _unpack_zip(filepath, destination_dir):
+    with ZipFile(filepath, 'r') as file:
+        file.extractall(destination_dir)
+
+
+# TODO: not sure there are actual 7z - maybe we don't need this
+def _unpack_7z(filepath, destination_dir):
+    call_string = " ".join([in_quotes(PATH['z7']), "e", filepath, "-o" + destination_dir, "-y"])
+    terminal(call_string)
+
 
 def in_quotes(str):
     return '"' + str + '"'
