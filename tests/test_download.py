26791019e7dbeee813b444a438bee4000972cb67
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2016-02-01 23:37:07 +0500

    Optimized downloading

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,18 +1,15 @@
+from datetime import date
 import os
 from unittest.mock import MagicMock
-
-from arrow import Arrow
 
 from cbr_db.download import download, get_date
 
 
 def test_get_last_modified(mocker):
     mock = MagicMock()
-    mock.info = MagicMock(return_value={
-        'Last-Modified': 'Sat 16 Jan 2016 18:47:00'
-    })
-    mocker.patch('urllib.request.urlopen', return_value=mock)
-    assert get_date('http://example.com') == Arrow(2016, 1, 16, 18, 47)
+    mock.getheader = lambda x: 'Sat 16 Jan 2016 18:47:00'
+    mocker.patch('cbr_db.download.urlopen', return_value=mock)
+    assert get_date('http://example.com') == date(2016, 1, 16)
 
 
 def test_download(mocker, tempdir):
@@ -21,9 +18,14 @@ def test_download(mocker, tempdir):
     input_file_path = os.path.join(tempdir, 'remote_file.txt')
     with open(input_file_path, 'w') as file:
         file.write('Hello world!\nThis is a test.\n')
-    mocker.patch('urllib.request.urlopen', lambda x: open(input_file_path, 'rb'))
-    mocker.patch('cbr_db.download.get_date',
-                 lambda x: Arrow(2016, 1, 16) if x.startswith('http://') else Arrow(2016, 1, 15))
+
+    def mock_urlopen(url):
+        mock = open(input_file_path, 'rb')
+        mock.getheader = lambda x: '2016-01-16'
+        return mock
+
+    mocker.patch('cbr_db.download.urlopen', mock_urlopen)
+    mocker.patch('cbr_db.download.get_date', lambda x: date(2016, 1, 16))
     download('http://localhost/path/to/file', dir_=output_dir, force=False, verbose=True)
     # Make sure downloaded file is correct
     output_file_path = os.path.join(output_dir, 'file')
@@ -31,10 +33,9 @@ def test_download(mocker, tempdir):
         assert file.read() == 'Hello world!\nThis is a test.\n'
     output_file_mtime = os.path.getmtime(output_file_path)
     # Try second time - must not actually download
-    mocker.patch('urllib.request.urlopen',
+    mocker.patch('cbr_db.download.urlopen',
                  side_effect=AssertionError('urlopen must not be called'))
-    mocker.patch('cbr_db.download.get_date',
-                 lambda x: Arrow(2016, 1, 16) if x.startswith('http://') else Arrow(2016, 1, 16))
+    mocker.patch('cbr_db.download.get_date', lambda x: date(2016, 1, 16))
     download('http://localhost/path/to/file', dir_=output_dir, force=False, verbose=True)
     # Make sure file is not changed
     assert os.path.getmtime(output_file_path) == output_file_mtime
