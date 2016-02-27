9528f2fd8d5b2965e83d4c82ea5eb2980bf6d1e7
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-02-28 01:20:36 +0300

    style formatting for settings.py

--- settings.py
+++ settings.py
@@ -5,31 +5,44 @@ You can specify a different settings file
 using CBR_DB_SETTINGS environment variable.
 """
 
+# QUESTION: Where is CBR_DB_SETTINGS defined, how one can modify it?
+
 import os
 import platform
 
-_PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-_WINDOWS = (platform.system() == 'Windows')
+IS_WINDOWS = (platform.system() == 'Windows')
+
+#
+# MySQL
+#
 
 MYSQL_HOST = 'localhost'
 MYSQL_PORT = 3306
 MYSQL_USER = 'test_user'
 MYSQL_PASSWORD = 'test_password'
 MYSQL_PATH = []
-if _WINDOWS:
+if IS_WINDOWS:
     MYSQL_PATH.extend([
         'C:\\Program Files (x86)\\MySQL\\MySQL Server 5.7\\bin',
         'C:\\MySQL\\bin'
     ])
 
+#
+# Database names
+#
+    
 DB_NAME_RAW = 'dbf_db'
 DB_NAME_FINAL = 'cbr_db'
 
+#
+# Paths and directories
+#
+
+_PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(_PROJECT_DIR, 'data')
 OUTPUT_DIR = os.path.join(DATA_DIR, 'output')
 
-
-if _WINDOWS:
+if IS_WINDOWS:
     UNPACK_RAR_EXE = os.path.join(_PROJECT_DIR, 'bin', 'unrar.exe')
 else:
     UNPACK_RAR_EXE = 'unrar'
