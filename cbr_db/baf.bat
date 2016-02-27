d5a92decf17fdbe2533a57069598f2c6755b84c6
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-02-28 01:57:45 +0300

    Update baf.bat

--- cbr_db/baf.bat
+++ cbr_db/baf.bat
@@ -4,4 +4,4 @@ REM Allows to call 'baf reset database' instead of 'python bankform.py reset dat
 REM This saves a bit of time typing.
 set PYTHONPATH=%~dp0..
 set CBR_DB_SETTINGS=settings
-python -m cbr_db.bankform %1 %2 %3 %4 %5 %6 %7 %8
+python -m cbr_db.bankform %* 
