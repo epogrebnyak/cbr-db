aeaf5d54fa0fde95be24d9fe9e84acee38191fbf
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-02-29 22:20:01 +0300

    results running sample.bat

--- cbr_db/sample.bat
+++ cbr_db/sample.bat
@@ -1,10 +1,10 @@
-baf reset database raw
-baf reset database final    
-baf download     101 2015-01-01
-baf unpack       101 2015-01-01
-baf make csv     101 2015-01-01
-baf import csv   101 2015-01-01
-baf make dataset 101 2015-01-01 
-baf migrate dataset 101        
-baf make balance
-baf report balance --xls
+call baf reset database raw
+call baf reset database final    
+call baf download     101 2015-01-01
+call baf unpack       101 2015-01-01
+call baf make csv     101 2015-01-01
+call baf import csv   101 2015-01-01
+call baf make dataset 101 2015-01-01 
+call baf migrate dataset 101        
+call baf make balance
+call baf report balance --xls
