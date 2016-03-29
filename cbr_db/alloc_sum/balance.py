c4837bf85e41480f860c9fdd8866f4134b44e459
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-03-30 00:27:53 +0300

    making balance with MultiIndex

--- cbr_db/alloc_sum/balance.py
+++ cbr_db/alloc_sum/balance.py
@@ -1,18 +1,30 @@
-def import_alloc(xl_filename, sheet='alloc'):
-     """Import dataframe"""
-     pass    
+import pandas as pd
+import numpy as np
 
-def import_balance(xl_filename, sheet='balance'):
-     """Import dataframe"""
-     pass    
-
-def import_contos(xl_filename, sheet='conto'):
-     """Import dataframe"""
-     pass  
+REGN = 1010
+    
+def flatten(source_df, key_col):    
+    col_dict = {'2016-01-01':['20160101', '20160101_ir', '20160101_iv'],
+                '2015-10-01':['20151001', '20151001_ir', '20151001_iv']}
+                
+    df = pd.DataFrame()
+    for dt, cols in col_dict.items(): # '2016-01-01':             
+        df_ = source_df[[key_col] + cols]
+        df_['dt'] = dt
+        df_ = df_.rename(index=str, columns={cols[0]:'itogo', cols[1]:'ir', cols[2]:'iv'})
+        df = df.append(df_)
+    df['regn'] = REGN 
+    return df
+    
+def xl_import(xl_filename):
+    alloc = pd.read_excel(xl_filename, 'alloc')
+    f101 = pd.read_excel(xl_filename, 'conto')
+    balance = pd.read_excel(xl_filename, 'balance')
+    return (alloc, 
+            flatten(f101, 'conto') 
+            flatten(balance , 'line')) 
 
 def make_balance(f101, alloc):
-    # for algorithm see sheet 'comment' in 'summ2.xls' 
-    # https://github.com/epogrebnyak/cbr-db/blob/pandas/py3/pandas_interface.py#L177-L235
     pass
      
 def comp(df1, df2):
@@ -20,24 +32,41 @@ def comp(df1, df2):
      return True
 
 def balance_file_test(filename):
-    alloc = import_alloc(filename)    
-    f101 = import_contos(filename)
-    ref_balance = import_balance(filename)
-     
-    balance = male_balance(alloc, conto)
+    alloc, conto, ref_balance = xl_import(filename)   
+    balance = make_balance(alloc, conto)
     assert comp(df1=ref_balance, df2=balance)
      
 if __name__ == "__main__":
-    filename = 'summ2.xls'
+
+    alloc, f101, ref_balance = xl_import('summ2.xls')
     
-    alloc = import_alloc(filename)
-    f101 = import_contos(filename)
-    ref_balance = import_balance(filename)
-    # dataframes imported - intermediate result above
+    # Replicating SQL code below
+    # see also sheet 'comment' in 'summ2.xls' 
+    # and https://github.com/epogrebnyak/cbr-db/blob/pandas/py3/pandas_interface.py#L177-L235
+    '''    
+    SELECT  dt, line,
+            regn,
+            sum(   ir*mult) ir,
+            sum(   iv*mult) iv,
+            sum(itogo*mult) itogo
+    from alloc a left join f101 v on v.conto = a.conto
+    where v.conto is not null
+    group by dt, line, regn;
+    '''
     
-    balance = male_balance(alloc, conto)
-    assert comp(df1=ref_balance, df2=balance)
-    # final result 1 above
+    joined = alloc.merge(f101, on='conto', how='left')
+    # todo 3 (optional): must check if there are any non-zero part of f101 that does not have code in alloc
     
-    balance_file_test("summ.xls")
-    # final result 2 above
+    # ir, iv, itogo multipy with mult
+    joined['ir'] = joined['ir']*joined['mult']
+    joined['iv'] = joined['iv']*joined['mult']
+    joined['itogo'] = joined['itogo']*joined['mult']
+
+    grouped = joined.groupby(['dt', 'line', 'regn']).agg({'ir':np.sum,'iv':np.sum, 'itogo':np.sum})
+    
+    # todo 1: grouped has multiIndex, I want to have same flat structure as in *ref_balance*
+    balance = grouped
+    print(balance)
+    print(ref_balance)
+    
+    # todo 2: check that values in *balance* and *ref_balance* are the same in corresponding rows and columns   
